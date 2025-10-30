7a7c638990f5fb43f8ec79cb2292853e459a648b
raquelpwn <pamellaraquel52@gmail.com>
2025-10-30 19:03:57 -0400

    Adicionando SENSOR3 e SENSOR4 e atualizando a lógica do alarme

--- exemplo_1/exemploArduinoIDE_1/exemploArduinoIDE_1.ino
+++ exemplo_1/exemploArduinoIDE_1/exemploArduinoIDE_1.ino
@@ -1,15 +1,19 @@
-int SENSOR1=2;
-int SENSOR2=3;
+int SENSOR1=2; // Porta 1
+int SENSOR2=3; // Janela 1
+int SENSOR3=4; // Porta 2
+int SENSOR4=5; // Janela 2
 int BUZZER=8;
 int BOTAO=7;
-int LED_VERMELHO=9; // Armado
-int LED_VERDE=10;   // Desarmado
+int LED_VERMELHO=9; // Armado (armado e sensor aberto)
+int LED_VERDE=10;   // Tudo ok
 
 bool armado = false;
 
 void setup() {
   pinMode(SENSOR1, INPUT_PULLUP);
   pinMode(SENSOR2, INPUT_PULLUP);
+  pinMode(SENSOR3, INPUT_PULLUP);
+  pinMode(SENSOR4, INPUT_PULLUP);
   pinMode(BUZZER, OUTPUT);
   pinMode(BOTAO, INPUT_PULLUP);
   pinMode(LED_VERMELHO, OUTPUT);
@@ -17,7 +21,7 @@ void setup() {
 }
 
 void loop() {
-  // Alterna entre armado/desarmado quando o botão é pressionado
+  // Alterna entre armado/desarmado 
   if (digitalRead(BOTAO)== LOW){
     delay(200); // evita múltiplas leituras do mesmo clique
     armado = !armado; // inverte o estado
@@ -27,11 +31,13 @@ void loop() {
   // Leitura dos sensores
   int estado1 = digitalRead(SENSOR1);
   int estado2 = digitalRead(SENSOR2);
-
-  // Sistema armado
+  int estado3 = digitalRead(SENSOR3);
+  int estado4 = digitalRead(SENSOR4);
+  
   if(armado){
-    if(estado1 == LOW || estado2 == LOW){
-      digitalWrite(LED_VERMELHO, HIGH); // alerta
+    // Se qualquer sensor estiver ABERTO (LOW)
+    if(estado1 == LOW || estado2 == LOW || estado3 == LOW || estado4 == LOW){
+      digitalWrite(LED_VERMELHO, HIGH);
       digitalWrite(LED_VERDE, LOW);
       digitalWrite(BUZZER, HIGH);
     }else{
@@ -43,7 +49,7 @@ void loop() {
 // Sistema DESARMADO
 else{
   digitalWrite(LED_VERMELHO, LOW);
-  digitalWrite(LED_VERDE, HIGH); // sistema desligado
+  digitalWrite(LED_VERDE, HIGH);
   digitalWrite(BUZZER, LOW);
 }
 
